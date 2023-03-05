0235aa5bf5dfa13c5218aa418d6ec493dd66445a
DayanaPlua <85946295+DayanaPlua@users.noreply.github.com>
2023-03-05 17:09:13 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,73 +1,180 @@
 import logging
 import os
+import base64
+import cStringIO
+import sys
+import tempfile
 
-from flask import Flask, render_template, request
-import google.cloud.logging
-from google.cloud import firestore
-from google.cloud import storage
+MODEL_BASE = '/opt/models/research'
+sys.path.append(MODEL_BASE)
+sys.path.append(MODEL_BASE + '/object_detection')
+sys.path.append(MODEL_BASE + '/slim')
 
-client = google.cloud.logging.Client()
-client.get_default_handler()
-client.setup_logging()
+from decorator import requires_auth
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask_wtf.file import FileField
+import numpy as np
+from PIL import Image
+from PIL import ImageDraw
+import tensorflow as tf
+from utils import label_map_util
+from werkzeug.datastructures import CombinedMultiDict
+from wtforms import Form
+from wtforms import ValidationError
+
 
 app = Flask(__name__)
 
 
+@app.before_request
+@requires_auth
+def before_request():
+  pass
+
+
+PATH_TO_CKPT = '/opt/graph_def/frozen_inference_graph.pb'
+PATH_TO_LABELS = MODEL_BASE + '/object_detection/data/mscoco_label_map.pbtxt'
+
+content_types = {'jpg': 'image/jpeg',
+                 'jpeg': 'image/jpeg',
+                 'png': 'image/png'}
+extensions = sorted(content_types.keys())
+
+
+def is_image():
+  def _is_image(form, field):
+    if not field.data:
+      raise ValidationError()
+    elif field.data.filename.split('.')[-1].lower() not in extensions:
+      raise ValidationError()
+
+  return _is_image
+
+
+class PhotoForm(Form):
+  input_photo = FileField(
+      'File extension should be: %s (case-insensitive)' % ', '.join(extensions),
+      validators=[is_image()])
+
+
+class ObjectDetector(object):
+
+  def __init__(self):
+    self.detection_graph = self._build_graph()
+    self.sess = tf.Session(graph=self.detection_graph)
+
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(
+        label_map, max_num_classes=90, use_display_name=True)
+    self.category_index = label_map_util.create_category_index(categories)
+
+  def _build_graph(self):
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+      od_graph_def = tf.GraphDef()
+      with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    return detection_graph
+
+  def _load_image_into_numpy_array(self, image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+  def detect(self, image):
+    image_np = self._load_image_into_numpy_array(image)
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+
+    graph = self.detection_graph
+    image_tensor = graph.get_tensor_by_name('image_tensor:0')
+    boxes = graph.get_tensor_by_name('detection_boxes:0')
+    scores = graph.get_tensor_by_name('detection_scores:0')
+    classes = graph.get_tensor_by_name('detection_classes:0')
+    num_detections = graph.get_tensor_by_name('num_detections:0')
+
+    (boxes, scores, classes, num_detections) = self.sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+
+    boxes, scores, classes, num_detections = map(
+        np.squeeze, [boxes, scores, classes, num_detections])
+
+    return boxes, scores, classes.astype(int), num_detections
+
+
+def draw_bounding_box_on_image(image, box, color='red', thickness=4):
+  draw = ImageDraw.Draw(image)
+  im_width, im_height = image.size
+  ymin, xmin, ymax, xmax = box
+  (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                ymin * im_height, ymax * im_height)
+  draw.line([(left, top), (left, bottom), (right, bottom),
+             (right, top), (left, top)], width=thickness, fill=color)
+
+
+def encode_image(image):
+  image_buffer = cStringIO.StringIO()
+  image.save(image_buffer, format='PNG')
+  imgstr = 'data:image/png;base64,{:s}'.format(
+      base64.b64encode(image_buffer.getvalue()))
+  return imgstr
+
+
+def detect_objects(image_path):
+  image = Image.open(image_path).convert('RGB')
+  boxes, scores, classes, num_detections = client.detect(image)
+  image.thumbnail((480, 480), Image.ANTIALIAS)
+
+  new_images = {}
+  for i in range(num_detections):
+    if scores[i] < 0.7: continue
+    cls = classes[i]
+    if cls not in new_images.keys():
+      new_images[cls] = image.copy()
+    draw_bounding_box_on_image(new_images[cls], boxes[i],
+                               thickness=int(scores[i]*10)-4)
+
+  result = {}
+  result['original'] = encode_image(image.copy())
+
+  for cls, new_image in new_images.iteritems():
+    category = client.category_index[cls]['name']
+    result[category] = encode_image(new_image)
+
+  return result
+
+
 @app.route('/')
-def root():
-    return render_template('home.html')
-
-
-@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    successful_upload = False
-    if request.method == 'POST':
-        uploaded_file = request.files.get('picture')
-
-        if uploaded_file:
-            gcs = storage.Client()
-            bucket = gcs.get_bucket(os.environ.get('BUCKET', 'my-bmd-bucket'))
-            blob = bucket.blob(uploaded_file.filename)
-
-            blob.upload_from_string(
-                uploaded_file.read(),
-                content_type=uploaded_file.content_type
-            )
-
-            logging.info(blob.public_url)
-
-            successful_upload = True
-
-    return render_template('upload_photo.html', 
-                           successful_upload=successful_upload)
+  photo_form = PhotoForm(request.form)
+  return render_template('upload.html', photo_form=photo_form, result={})
 
 
-@app.route('/search')
-def search():
-    query = request.args.get('q')
-    results = []
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+  form = PhotoForm(CombinedMultiDict((request.files, request.form)))
+  if request.method == 'POST' and form.validate():
+    with tempfile.NamedTemporaryFile() as temp:
+      form.input_photo.data.save(temp)
+      temp.flush()
+      result = detect_objects(temp.name)
 
-    if query:
-        db = firestore.Client()
-        doc = db.collection(u'tags').document(query.lower()).get().to_dict()
-
-        try:
-            for url in doc['photo_urls']:
-                results.append(url)
-        except TypeError as e:
-            pass
-
-    return render_template('search.html', query=query, results=results)
+    photo_form = PhotoForm(request.form)
+    return render_template('upload.html',
+                           photo_form=photo_form, result=result)
+  else:
+    return redirect(url_for('upload'))
 
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return render_template('error.html'), 500
+client = ObjectDetector()
 
 
 if __name__ == '__main__':
-    app.run(debug=True,
-            host='0.0.0.0',
-            port=int(os.environ.get('PORT', 8080)))
-
+  app.run(host='0.0.0.0', port=80, debug=False)
